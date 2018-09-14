4355f9bae86a843047b31abd0c6d0fd87113b278
Justin Grierson <ju3tin@hotmail.co.uk>
2018-09-14 10:45:12 +0100

    sdfsdf

--- shop1/connections.php
+++ shop1/connections.php
@@ -7,11 +7,11 @@
       
     // connect to mysql 
       
-    mysql_connect($server, $user, $pass) or die("Sorry, can't connect to the mysql."); 
+    mysqli_connect($server, $user, $pass) or die("Sorry, can't connect to the mysql."); 
       
     // select the db 
       
-    mysql_select_db($db) or die("Sorry, can't select the database."); 
+    mysqli_select_db($db) or die("Sorry, can't select the database."); 
   
 ?>
 
