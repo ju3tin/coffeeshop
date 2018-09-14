f88543f97167d8ba234cbe26e31c27a70e6b1874
Justin Grierson <ju3tin@hotmail.co.uk>
2018-09-14 10:53:02 +0100

    sdfsdf

--- shop1/includes/connection.php
+++ shop1/includes/connection.php
@@ -1,18 +1,17 @@
- <?php 
-  
-    $server="eu-cdbr-west-02.cleardb.net"; 
-    $user="be1251cea96efe"; 
-    $pass="f9393f52"; 
-    $db="heroku_421c90367cd9a12"; 
-      
-    // connect to mysql 
-      
-    mysqli_connect($server, $user, $pass) or die("Sorry, can't connect to the mysql."); 
-      
-    // select the db 
-      
-    mysqli_select_db($db) or die("Sorry, can't select the database."); 
-  
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'eu-cdbr-west-02.cleardb.net');
+define('DB_USERNAME', 'be1251cea96efe');
+define('DB_PASSWORD', 'f9393f52');
+define('DB_NAME', 'heroku_421c90367cd9a12');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 ?>
-
 
