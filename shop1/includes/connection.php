6d8adc8503d45962819a01f54c26e63a9a9b7b4f
Justin Grierson <ju3tin@hotmail.co.uk>
2018-09-14 10:47:15 +0100

    sdfsdf

--- shop1/includes/connection.php
+++ shop1/includes/connection.php
@@ -1,16 +1,18 @@
-<?php
-
-	$server="localhost";
-	$user="tutorial";
-	$pass="supersecretpassword";
-	$db="tutorials";
-	
-	// connect to mysql
-	
-	mysql_connect($server, $user, $pass) or die("Sorry, can't connect to the mysql.");
-	
-	// select the db
-	
-	mysql_select_db($db) or die("Sorry, can't select the database.");
-
+ <?php 
+  
+    $server="eu-cdbr-west-02.cleardb.net"; 
+    $user="be1251cea96efe"; 
+    $pass="f9393f52@"; 
+    $db="heroku_421c90367cd9a12"; 
+      
+    // connect to mysql 
+      
+    mysqli_connect($server, $user, $pass) or die("Sorry, can't connect to the mysql."); 
+      
+    // select the db 
+      
+    mysqli_select_db($db) or die("Sorry, can't select the database."); 
+  
 ?>
+
+
